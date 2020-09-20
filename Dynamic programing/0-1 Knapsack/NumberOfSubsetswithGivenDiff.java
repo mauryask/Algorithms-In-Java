15e2684bccbad2349d4d38468f70fb2299dd27c7
BlackHatos <pnssoftwares7@gmail.com>
2020-09-20 16:46:59 +0530

    UPadtedd

---
+++ Dynamic programing/0-1 Knapsack/NumberOfSubsetswithGivenDiff.java
@@ -0,0 +1,45 @@
+/**
+* How many ways a set can be divided into two such subsets such that
+* diferene between the sum of elements can produce the gievn value(diff)
+*/
+
+import static java.lang.System.*;
+
+public class NumberOfSubsetswithGivenDiff
+{
+	public static void main(String [] args)
+	{
+		int set[] = {1,2,7};
+		int n = set.length;
+		int diff = 4;
+		int sum = 0;
+		for(int i=0; i<n; i++)
+		sum += set[i];
+	
+	   // find number of substes that can produce this sum
+	    float s1 = (sum + (float) diff) / 2;
+	
+	    out.println(solve(set, n, s1));
+	}
+	
+	static int solve(int set[], int n, float s)
+	{
+		int dp[][] = new int[n+1][(int)s+1];
+		for(int i=0; i<n+1; i++)
+			dp[i][0] = 1;
+		
+		
+		for(int i=1;i<n+1; i++)
+		{
+			for(int j=1;j<s+1; j++)
+			{
+				if(set[i-1] <=  j)
+					dp[i][j] = dp[i-1][j-set[i-1]] + dp[i-1][j];
+				else
+					dp[i][j] = dp[i-1][j];
+			}
+		}
+		
+		return dp[n][(int)s];
+	}
+}
