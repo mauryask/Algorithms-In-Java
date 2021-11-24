e0273add94f135f0909fd539b4d558499c59afa9
shubhammaurya <pnssoftwares7@gmail.com>
2021-11-24 11:52:01 +0530

    Updated

--- Recursion & BackTracking/NQueensProblem.java
+++ Recursion & BackTracking/NQueensProblem.java
@@ -1,38 +1,60 @@
+/*
+* Given N*N board and N-queens 
+* Place the queen in such a way so that no 
+* two queens are under attack 
+***
+Queen can move 
+*********************
+1) horizontally
+2) vertically 
+3) diagonally (both diagonal)
+***********
+Two queens will be under attack if: 
+-> they are in same row 
+-> same coloumn 
+-> same diagonal
+*/
+
 import static java.lang.System.*;
+import java.util.*;
+
 public class NQueensProblem
 {
+    static int count  = 0;
 	public static void main(String [] args)
 	{
-		int n = 4;
+		int n = 5;
+		// use the below varibale only if you want all the possible solution
+		boolean res = false; //by default assume there is no solution : false
 		int board[][] = new int[n][n];
-		out.println("\n "+placeTheQueen(board,0,n));
+		out.println("Is solution possible: "+placeTheQueen(board,0,n, res));
+		out.println("Total solution: "+count);
 	}
 	
-	static boolean placeTheQueen(int board[][], int col,int n)
+	static boolean placeTheQueen(int board[][], int col,int n/*print all solution take res here*/,boolean res)
 	{
 		if(col==n)
 		{
 			printSol(board,n);
+			count++;
+			out.println("\n********\n");
 			return true;
 		}
 		
-		/// if you want to print all the possible solutions
-		//  boolean res = false; 
-		 
 		for(int i=0;i<n; i++)
 		{
 		   if(isSafe(board, i, col, n))
 		   {
 			   board[i][col] = 1;
-			   //res = placeTheQueen(board,col+1, n);
-			   if(placeTheQueen(board, col+1, n))
-				   return true;
+			   res = placeTheQueen(board,col+1, n, res);
+			   /*if(placeTheQueen(board, col+1, n))
+				   return true;*/
 			   board[i][col] = 0;
 		   }			   
 		}
 		
-		//return res;
-		return false;
+		return res;
+		//return false;
 	}
 	
 	static boolean isSafe(int board[][],int row,int col, int n)
@@ -57,7 +79,6 @@ public class NQueensProblem
 	
 	static void printSol(int board[][], int n)
 	{
-    	out.println("\n***************");
 		for(int[] x : board)
 		{
 			for(int y : x)
---
+++ Recursion & BackTracking/SudokuSolver.java
@@ -0,0 +1,111 @@
+/*
+* T(n) : O(8^n)
+* S(n) : O(n*n)
+*/
+
+import static java.lang.System.*;
+import java.util.*;
+
+public class SudokuSolver 
+{
+	static void solveSudoku(int[][] board, int i, int j,int n)
+	{
+		// Done with all the rows
+		if(i== n)
+		{
+			// if you will not print matrix here 
+			// you will not get corret sudoku 
+			// because of the backtracking 
+			// most of the values will be set to 0 again
+			printBoard(board);
+		    return;	
+		}
+		
+		int ni = 0;
+		int nj = 0;
+		
+		if(j==n-1)
+		{
+		  ni = i+1;
+          nj = 0;		  
+		}
+		else
+		{
+			ni = i;
+			nj = j+1;
+		}
+		
+		if(board[i][j] != 0)
+			solveSudoku(board, ni, nj, n);
+		else
+		{
+			for(int p=1; p<=9; p++)
+			{
+				if(isSafe(board, i, j, p, n))
+				{
+					board[i][j]  = p;
+					solveSudoku(board, ni, nj, n);
+					board[i][j] = 0;
+				}
+			}
+		}
+	}
+	
+	static boolean isSafe(int A[][], int i, int j,int k, int n)
+	{
+		// check in the row
+		for(int p=0; p<n; p++)
+			if(A[p][j] == k)
+			  return false;
+		  
+		// check in the column
+		for(int p=0; p<n; p++)
+			if(A[i][p] == k)
+			  return false;
+		
+		// finding index of the top-left element of the submatrix 
+		// in which the current element is present
+		int subi = 3*(i/3);
+		int subj = 3*(j/3);
+		
+		// check in the sub matrix
+		for(int p=0; p<3; p++)
+		{
+			for(int q=0; q<3; q++)
+			{
+				if(A[subi+p][subj+q] == k)
+				 return false;
+			}
+		}
+		
+		return true;
+	}
+	
+	
+	static void printBoard(int[][] board)
+	{
+		for(int x[] : board)
+		{
+			for(int y: x)
+				out.print(y+" ");
+			out.println();
+		}
+	}
+	
+	public static void main(String [] args)
+	{
+		int n = 9;
+		int board[][] =  
+		 {{3, 0, 6, 5, 0, 8, 4, 0, 0}, 
+         {5, 2, 0, 0, 0, 0, 0, 0, 0}, 
+         {0, 8, 7, 0, 0, 0, 0, 3, 1}, 
+         {0, 0, 3, 0, 1, 0, 0, 8, 0}, 
+         {9, 0, 0, 8, 6, 3, 0, 0, 5}, 
+         {0, 5, 0, 0, 9, 0, 6, 0, 0}, 
+         {1, 3, 0, 0, 0, 0, 2, 5, 0}, 
+         {0, 0, 0, 0, 0, 0, 0, 7, 4}, 
+         {0, 0, 5, 2, 0, 6, 3, 0, 0}};
+		 
+		solveSudoku(board,0,0,9);
+	}
+}
