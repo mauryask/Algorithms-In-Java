bc679e8c8da88cb47af67ce32a6d452a0493a897
mauryask <mauryask1732@gmail.com>
2025-03-02 12:52:34 +0530

    Updated

--- Sliding Window/CountOcuuranceOfAnagras.java
+++ Sliding Window/CountOcuuranceOfAnagras.java
@@ -1,178 +1,132 @@
 /*
-* Time complexity  : O(n*k)
-* Space complexity : O(k) as the map
-********************
 ** fixed size window
+// https://www.geeksforgeeks.org/problems/count-occurences-of-anagrams5839/1
 */
+
+// T(n) : O(n)
+// S(n) : O(n)
+
 import static java.lang.System.*;
 import java.util.*;
 
-public class CountOcuuranceOfAnagras
-{
-	static int anagramCount(String str, String pattern,
-	int n,int k)
-	{
-		/*
-		* this map conatins charcater 
-		* count of pattern string
-		*/
-		
-		Map<Character, Integer> map = new HashMap<>();
-	
-		for(int i = 0; i<k; i++)
-		{
-			char c1 = pattern.charAt(i);
-			map.put(c1, map.getOrDefault(c1, 0)+1);
-		}			
-				
-	    int i=0, j=0;
-		int count = 0;
-		
-		/*
-		* this map conatins charcater 
-		* count of main string (str)
-		*/		
-		Map<Character, Integer> m1 = new HashMap<>();
-		
-		while(j<n)
-		{
-			char c2 = str.charAt(j);
-			m1.put(c2, m1.getOrDefault(c2, 0)+1);
-			
-			if(j-i+1 == k)
-			{
-				boolean flag = true;
-				
-				/*
-				*  Check if the window
-				/* contains same charcaters
-				/* in same amount or not
-				*/
-				for(Map.Entry<Character, Integer>
-				m : map.entrySet())
-				{
-					char ch = m.getKey();
-					int cnt = m.getValue();
-					
-					/*
-					* If any charcater count 
-					* is not same in m1 as in map
-					* set flag false and terminate loop
-					***********
-					* Here getOrDefault() becasuse if element
-					* is not pr4sent in m1 then it
-					* will give null pointer exception
-					*/
-					
-					if(m1.getOrDefault(ch, 0) != cnt)
-					{
-						flag = false;
-						break;
-					}
-				}
-				
-				/*
-				* if flag true means
-				* the window contains same number of
-				* character as in pattern
-				*/
-				
-				if(flag)
-					count++;
-			
-				char c3 = str.charAt(i);
-				
-				
-				/*
-				* Decrease the count of ith character
-				* from m1 before sliding to the next window
-				* ******************************
-				* as we do int max sum sub array
-				* sum = sum - Ai
-				*/
-				m1.replace(c3, m1.get(c3)-1);
-				i++;
-			}
-			
-			j++;
-		}
-			
-		return count;
-	}
-	
-	
-	/**
-	* T(n) = (n*k)
-	* S(n) = O(k)
-	*/
-	
-	static int bruteForce(String str, String pattern,
-	int n, int k)
-	{
-		Map<Character, Integer> map = new HashMap<>();
-		Map<Character, Integer> m1;
-		
-		for(int i=0; i<k; i++)
-		{
-			char ch = pattern.charAt(i);
-			map.put(ch, map.getOrDefault(ch, 0)+1);
-		}
-		
-		int anagramCount = 0;
-		
-		for(int i=0; i<= n-k ; i++)
-		{
-			m1 = new HashMap<>();
-		
-			for(int j=i; j<i+k; j++)
-			{
-				char ch = str.charAt(j);
-				m1.put(ch, m1.getOrDefault(ch, 0)+1);
-			}
+public class CountOcuuranceOfAnagras {
 
-			boolean flag = true;
-			
-			for(Map.Entry<Character, Integer> m 
-			: map.entrySet())
-			{
-				char key = m.getKey();
-				int  value = m.getValue();
-				
-				/*
+   static int anagramCount(String pat, String txt) 
+   {
+		int n = txt.length();
+		int k = pat.length();
+
+        if(n < k){
+			return -1; // Anagram is not possible 
+		}
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < k; i++) {
+            char ch = pat.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        int count = map.size(); // Count of total unique charcaters in pattrern
+        int ans = 0; 
+        int i = 0, j = 0;
+
+
+        while (j < n) {
+            char ch = txt.charAt(j);
+
+            if (map.containsKey(ch)) {
+                map.replace(ch, map.get(ch) - 1);
+
+                if (map.get(ch) == 0) {
+                    count--;
+                }
+            }
+
+            if (j - i + 1 == k) {
+                // If count is 0: means we got all the charcaters of pattern
+				// in the current window
+				if (count == 0) {
+                    ans++;
+                }
+
+                // Undo the changes at index i
+                ch = txt.charAt(i);
+
+                if (map.containsKey(ch)) {
+                    int value = map.get(ch);
+                    map.replace(ch, value + 1);
+                    // If frequency of character changes from 0 to non-zero
+					// Increase the count by 1
+					// That means we need to find count instances of this charcater
+                    if (value == 0) {
+                        count++;
+                    }
+                }
+
+                i++;
+            }
+            
+            j++;
+        }
+
+        return ans;
+    }
+
+    /**
+     * T(n) = (n*k) S(n) = O(k)
+     */
+    static int bruteForce(String str, String pattern, int n, int k) {
+        Map<Character, Integer> map = new HashMap<>();
+        Map<Character, Integer> m1;
+
+        for (int i = 0; i < k; i++) {
+            char ch = pattern.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        int anagramCount = 0;
+
+        for (int i = 0; i <= n - k; i++) {
+            m1 = new HashMap<>();
+
+            for (int j = i; j < i + k; j++) {
+                char ch = str.charAt(j);
+                m1.put(ch, m1.getOrDefault(ch, 0) + 1);
+            }
+
+            boolean flag = true;
+
+            for (Map.Entry<Character, Integer> m
+                    : map.entrySet()) {
+                char key = m.getKey();
+                int value = m.getValue();
+
+                /*
 				* getOrDefault() here becasuse
 				* if key is not present in m1
 				* then it will retun null
 				* as the resuolt null pointer exception
 				* wil be thrown
-				*/
-				if(m1.getOrDefault(key, 0) != value)
-				{
-					flag = false;
-					break;
-				}
-			}
-			
-			if(flag)
-				anagramCount++;
-		}
-		
-		return anagramCount;
-	}
-	
-	public static void main(String [] args)
-	{
-		String str = "aababddeabaa";
-		String pattern = "aaba";
-		int k = pattern.length(); // window size
-		int n = str.length();
-		
-		if(n < k)
-		{
-			out.println("anagarms are not possible!!");
-			System.exit(0);
-		}
-		
-		 out.println(anagramCount(str, pattern, n, k));
-		//out.println(bruteForce(str, pattern, n, k));
-	}
+                 */
+                if (m1.getOrDefault(key, 0) != value) {
+                    flag = false;
+                    break;
+                }
+            }
+
+            if (flag) {
+                anagramCount++;
+            }
+        }
+
+        return anagramCount;
+    }
+
+    public static void main(String[] args) {
+        String str = "aababddeabaa";
+        String pattern = "aaba";
+        out.println(anagramCount(pattern, str));
+        //out.println(bruteForce(str, pattern, n, k));
+    }
 }
