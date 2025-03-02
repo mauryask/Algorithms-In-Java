ff6eb64454aea0dab57c068313bec19a8afbbaad
mauryask <mauryask1732@gmail.com>
2025-03-02 13:20:22 +0530

    Updated

--- Sliding Window/CountOcuuranceOfAnagras.java
+++ Sliding Window/CountOcuuranceOfAnagras.java
@@ -1,24 +1,21 @@
 /*
 ** fixed size window
 // https://www.geeksforgeeks.org/problems/count-occurences-of-anagrams5839/1
-*/
+ */
 
 // T(n) : O(n)
 // S(n) : O(n)
-
-import static java.lang.System.*;
 import java.util.*;
 
 public class CountOcuuranceOfAnagras {
 
-   static int anagramCount(String pat, String txt) 
-   {
-		int n = txt.length();
-		int k = pat.length();
+    static int anagramCount(String pat, String txt) {
+        int n = txt.length();
+        int k = pat.length();
 
-        if(n < k){
-			return -1; // Anagram is not possible 
-		}
+        if (n < k) {
+            return -1; // Anagram is not possible 
+        }
 
         Map<Character, Integer> map = new HashMap<>();
 
@@ -28,9 +25,8 @@ public class CountOcuuranceOfAnagras {
         }
 
         int count = map.size(); // Count of total unique charcaters in pattrern
-        int ans = 0; 
+        int ans = 0;
         int i = 0, j = 0;
-
 
         while (j < n) {
             char ch = txt.charAt(j);
@@ -45,8 +41,8 @@ public class CountOcuuranceOfAnagras {
 
             if (j - i + 1 == k) {
                 // If count is 0: means we got all the charcaters of pattern
-				// in the current window
-				if (count == 0) {
+                // in the current window
+                if (count == 0) {
                     ans++;
                 }
 
@@ -57,8 +53,8 @@ public class CountOcuuranceOfAnagras {
                     int value = map.get(ch);
                     map.replace(ch, value + 1);
                     // If frequency of character changes from 0 to non-zero
-					// Increase the count by 1
-					// That means we need to find count instances of this charcater
+                    // Increase the count by 1
+                    // That means we need to find count instances of this charcater
                     if (value == 0) {
                         count++;
                     }
@@ -66,7 +62,7 @@ public class CountOcuuranceOfAnagras {
 
                 i++;
             }
-            
+
             j++;
         }
 
@@ -74,59 +70,65 @@ public class CountOcuuranceOfAnagras {
     }
 
     /**
-     * T(n) = (n*k) S(n) = O(k)
+     * T(n) = (n*k) 
+	 * S(n) = O(k)
      */
-    static int bruteForce(String str, String pattern, int n, int k) {
+	
+    static int bruteForce(String str, String pattern) 
+	{
+        int n = str.length();
+        int k = pattern.length();
+
+        if (n < k) {
+            return -1;
+        }
+
         Map<Character, Integer> map = new HashMap<>();
-        Map<Character, Integer> m1;
 
         for (int i = 0; i < k; i++) {
             char ch = pattern.charAt(i);
             map.put(ch, map.getOrDefault(ch, 0) + 1);
         }
 
-        int anagramCount = 0;
+        int ans = 0;
 
-        for (int i = 0; i <= n - k; i++) {
-            m1 = new HashMap<>();
+        for (int i = 0; i <= n - k; i++) 
+		{
+             for (int j = i; j < n; j++)
+			 {
+                if (j - i + 1 == k) {
+                    Map<Character, Integer> m = new HashMap<>();
+                   
+				    for (int p = i; p <= j; p++) {
+                        char ch = str.charAt(p);
+                        m.put(ch, m.getOrDefault(ch, 0) + 1);
+                    }
 
-            for (int j = i; j < i + k; j++) {
-                char ch = str.charAt(j);
-                m1.put(ch, m1.getOrDefault(ch, 0) + 1);
-            }
+                    boolean flag = true;
 
-            boolean flag = true;
+                    for (char key : map.keySet()) {
+                        if (!(m.containsKey(key) && Objects.equals(m.get(key), map.get(key)))) {
+                            flag = false;
+							break;
+                        }
+                    }
 
-            for (Map.Entry<Character, Integer> m
-                    : map.entrySet()) {
-                char key = m.getKey();
-                int value = m.getValue();
+                    if (flag) {
+                        ans++;
+                    }
 
-                /*
-				* getOrDefault() here becasuse
-				* if key is not present in m1
-				* then it will retun null
-				* as the resuolt null pointer exception
-				* wil be thrown
-                 */
-                if (m1.getOrDefault(key, 0) != value) {
-                    flag = false;
-                    break;
+					break;
                 }
-            }
-
-            if (flag) {
-                anagramCount++;
             }
         }
 
-        return anagramCount;
+        return ans;
     }
 
     public static void main(String[] args) {
         String str = "aababddeabaa";
         String pattern = "aaba";
-        out.println(anagramCount(pattern, str));
-        //out.println(bruteForce(str, pattern, n, k));
+        // System.out.println(anagramCount(pattern, str));
+        System.out.println(bruteForce(str, pattern));
     }
 }
